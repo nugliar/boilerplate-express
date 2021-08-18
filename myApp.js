3c434eb591b9d705cfbaf57bcb3ae69721533465
nugliar <hashan.hall@gmail.com>
2021-08-18 11:33:26 -0500

    serve string

--- myApp.js
+++ myApp.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express')
-})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
